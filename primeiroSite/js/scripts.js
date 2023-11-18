187e8a1d96793d8f60a5faabbba51236b90c9658
patimaltez <patricia.izunome@gmail.com>
2023-11-18 11:12:36 -0300

    alterações
    alterações

---
+++ primeiroSite/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ 
+  $('.desenho-001').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
+  
+});
